f17691a2c9a4ce5285aaa1fa971d08f35748af25
gqqnbig <gqqnb2005@gmail.com>
2018-05-27 22:26:15 -0700

    添加单元测试

---
+++ Test/TestMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using uMatrixCleaner;
+using Xunit;
+
+namespace Test
+{
+    public class TestMain
+    {
+        [Fact]
+        public void TestFindConflictingRule()
+        {
+            var rule = new UMatrixRule("* www.google-analytics.com image allow");
+            var rules = new List<UMatrixRule>(new[]
+            {
+                new UMatrixRule("* * image block"),
+                new UMatrixRule("* 1st-party image block"),
+            });
+            rules.Add(rule);
+
+
+            var cr = Program.FindConflictingRule(rule, rules);
+            Assert.Equal("* 1st-party image block", cr.ToString());
+        }
+    }
+}
