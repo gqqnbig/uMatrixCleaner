c3a2d57df0504020c4c3ebdc71b67479cc6c7375
gqqnbig <gqqnb2005@gmail.com>
2018-05-28 12:10:56 -0700

    实现GetClosestParent

--- Test/TestMain.cs
+++ Test/TestMain.cs
@@ -9,19 +9,19 @@ namespace Test
     public class TestMain
     {
         [Fact]
-        public void TestFindConflictingRule()
+        public void TestGetClosestParent()
         {
             var rule = new UMatrixRule("* www.google-analytics.com image allow");
             var rules = new List<UMatrixRule>(new[]
             {
-                new UMatrixRule("* * image block"),
-                new UMatrixRule("* 1st-party image block"),
+                new UMatrixRule("* * image allow"),
+                new UMatrixRule("* 1st-party image allow"),
             });
             rules.Add(rule);
 
 
-            var cr = Program.FindConflictingRule(rule, rules);
-            Assert.Equal("* 1st-party image block", cr.ToString());
+            var cr = Program.GetClosestParent(rule, rules);
+            Assert.Equal("* 1st-party image allow", cr.ToString());
         }
     }
 }
--- uMatrixCleaner/Program.cs
+++ uMatrixCleaner/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace uMatrixCleaner
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -28,43 +30,14 @@ namespace uMatrixCleaner
 
             throw new NotImplementedException();
         }
-    }
 
-    [DebuggerDisplay("{Source, nq} {Target, nq} {Type, nq} {IsAllow?\"allow\":\"block\", nq}")]
-    class UMatrixRule
-    {
-        public string Source { get; set; }
 
-        public string Target { get; set; }
 
-        public Type Type { get; set; }
-
-        public bool IsAllow { get; set; }
-
-        public UMatrixRule(string line)
+        public static UMatrixRule GetClosestParent(UMatrixRule rule, ICollection<UMatrixRule> list)
         {
-            string[] parts = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-            Source = parts[0];
-            Target = parts[1];
-            Type = parts[2] == "*" ? Type.All : (Type)Enum.Parse(typeof(Type), parts[2], true);
-            IsAllow = parts[3] == "allow";
+            var closestParent = list.Where(r => r.Equals(rule) == false && r.Covers(rule).GetValueOrDefault(true))
+                                .OrderByDescending(r => r.Specificity).FirstOrDefault();
+            return closestParent;
         }
     }
-
-    [Flags]
-    enum Type
-    {
-        Cookie = 1,
-        Css = 2,
-        Image = 4,
-        Media = 8,
-        Script = 16,
-        Xhr = 32,
-        Frame = 64,
-        Other = 128,
-        All = 255
-
-    }
-
-
 }
