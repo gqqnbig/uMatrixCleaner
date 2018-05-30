4d9251e45ba808974bfe73f1ba301ee2ae129fb5
gqqnbig <gqqnb2005@gmail.com>
2018-05-30 01:20:43 -0700

    修复数据类型，Contains不返回null了

--- Tests/TestUMatrixRule.cs
+++ Tests/TestUMatrixRule.cs
@@ -19,7 +19,7 @@ namespace Tests
         [InlineData("* * css", "* 1st-party *", true)]
         [InlineData("* 1st-party other", "wenku.baidu.com baidu.com other", true)]
         [InlineData("* youtube.com *", "91porn.com 192.240.120.2 media", false)]
-        public void TestCovers(string r1, string r2, bool? result)
+        public void TestCovers(string r1, string r2, bool result)
         {
             var rule1 = new UMatrixRule(r1 + " block");
             var rule2 = new UMatrixRule(r2 + " block");
